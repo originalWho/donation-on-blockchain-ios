f296bf858ec692ad6ad299ce0686be9e6904e827
originalWho <da.erow@gmail.com>
2018-11-28 23:16:16 +0300

    Implemented state change behavior at view, added new connections at storyboard

--- BlockchainDonation/Sources/RIBs/Donation Chains List/View/DonationChainsListView.swift
+++ BlockchainDonation/Sources/RIBs/Donation Chains List/View/DonationChainsListView.swift
@@ -56,11 +56,14 @@ final class DonationChainsListViewImpl: UIViewController {
     private var expandedDonationChainsIDs: Set<DonationChainID> = []
 
     private var state: State = .default {
-        willSet {
-            validateStateTransition(from: state, to: newValue)
-        }
         didSet {
-            handleStateTransition(from: oldValue, to: state)
+            guard oldValue != state else {
+                return
+            }
+
+            UIView.animate(withDuration: 0.25) { [unowned self] in
+                self.handleStateTransition(from: oldValue, to: self.state)
+            }
         }
     }
 
@@ -85,53 +88,114 @@ final class DonationChainsListViewImpl: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureRefreshControl()
         setInitialViewConfiguration()
+        setDefaultViewConfiguration()
     }
 
     // MARK: - Private methods
 
+    private func configureRefreshControl() {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self,
+                                 action: #selector(onRefreshControlValueChanged(_:)),
+                                 for: .valueChanged)
+        tableView.refreshControl = refreshControl
+    }
+
     private func setInitialViewConfiguration() {
+        donationAmountTextField.placeholder = "How much?"
+//        donationDescriptionTextView.place
+    }
+
+    private func setDefaultViewConfiguration() {
         dimmerView.alpha = 0.0
+        donationAmountTextField.isEnabled = true
+        donationDescriptionTextView.isEditable = true
         registerFormContainerView.isHidden = true
+        donationAmountTextField.isHidden = false
+        donationDescriptionTextView.isHidden = false
         registerDoneTitleLabel.isHidden = true
         registerDoneDescriptionLabel.isHidden = true
         activityIndicator.isHidden = true
         cancelButton.isHidden = true
-    }
 
-    private func validateStateTransition(from oldState: State, to newState: State) {
-        switch (oldState, newState) {
-        case (.default, .donationFormVisible),
-             (.donationFormVisible, .default),
-             (.donationFormVisible, .donationFormProcessing),
-             (.donationFormProcessing, .donationFormDone),
-             (.donationFormDone, .default):
-            return
-
-        default:
-            assertionFailure("Invalid state transition from \(oldState) to \(newState)")
-        }
+        donateButton.setTitle("Donate", for: .normal)
     }
 
     private func handleStateTransition(from oldState: State, to newState: State) {
         switch (oldState, newState) {
         case (.default, .donationFormVisible):
-            return
+            dimmerView.alpha = 1.0
+            registerFormContainerView.isHidden = false
+            cancelButton.isHidden = false
+            donateButton.isEnabled = false
+            donateButton.setTitle("Send", for: .normal)
 
         case (.donationFormVisible, .default):
-            return
+            setDefaultViewConfiguration()
 
         case (.donationFormVisible, .donationFormProcessing):
-            return
+            donationAmountTextField.isEnabled = false
+            donationDescriptionTextView.isEditable = false
+            activityIndicator.isHidden = false
+            cancelButton.isHidden = true
+            donateButton.isEnabled = false
 
         case (.donationFormProcessing, .donationFormDone):
-            return
+            donationAmountTextField.isHidden = true
+            donationDescriptionTextView.isHidden = true
+            registerDoneTitleLabel.isHidden = false
+            registerDoneDescriptionLabel.isHidden = false
+            activityIndicator.isHidden = true
+            donateButton.isEnabled = true
+            donateButton.setTitle("Done", for: .normal)
 
         case (.donationFormDone, .default):
-            return
+            setDefaultViewConfiguration()
 
         default:
+            assertionFailure("Invalid state transition from \(oldState) to \(newState)")
             return
+        }
+
+        registerFormContainerStackView.layoutIfNeeded()
+    }
+
+    @objc private func onRefreshControlValueChanged(_ sender: UIRefreshControl) {
+        eventHandler?.onDonationChainsUpdateRequested()
+    }
+
+    @IBAction private func onDonateButtonPressed(_ sender: UIButton) {
+        switch state {
+        case .default:
+            eventHandler?.onShowRegisterDonationFormButtonPressed()
+
+        case .donationFormVisible:
+            guard 
+                let amount = donationAmountTextField.text, !amount.isEmpty,
+                let description = donationDescriptionTextView.text, !description.isEmpty
+            else {
+                return
+            }
+
+            eventHandler?.onRegisterDonationButtonPressed(amount: amount, description: description)
+
+        case .donationFormProcessing:
+            assertionFailure("Button should not be enabled in this state")
+
+        case .donationFormDone:
+            eventHandler?.onHideRegisterDonationFormButtonPressed()
+        }
+    }
+
+    @IBAction private func onCancelButtonPressed(_ sender: UIButton) {
+        switch state {
+        case .default, .donationFormProcessing, .donationFormDone:
+            assertionFailure("Button should not be available in this state")
+
+        case .donationFormVisible:
+            eventHandler?.onHideRegisterDonationFormButtonPressed()
         }
     }
 
@@ -141,28 +205,36 @@ final class DonationChainsListViewImpl: UIViewController {
 
 extension DonationChainsListViewImpl: DonationChainsListView {
 
-    func showDonationChainFullInfo(for identifier: DonationChainID) {
+    func update(with donationChains: [DonationChain]) {
+        self.donationChains = donationChains
+        tableView.reloadData() // TODO: Switch to animated updates
+        tableView.refreshControl?.endRefreshing()
+    }
 
+    func showDonationChainFullInfo(for identifier: DonationChainID) {
+        expandedDonationChainsIDs.insert(identifier)
+        tableView.reloadData() // TODO: Switch to animated updates
     }
 
     func hideDonationChainFullInfo(for identifier: DonationChainID) {
-
+        expandedDonationChainsIDs.remove(identifier)
+        tableView.reloadData() // TODO: Switch to animated updates
     }
 
     func showRegisterDonationForm() {
-
+        state = .donationFormVisible
     }
 
     func hideRegisterDonationForm() {
-
+        state = .default
     }
 
     func showDonationRegistrationProcessing() {
-
+        state = .donationFormProcessing
     }
 
     func showDonationRegistrationProcessingDone() {
-
+        state = .donationFormDone
     }
     
 }
@@ -217,10 +289,10 @@ extension DonationChainsListViewImpl: UITableViewDelegate {
 
         let donationChainID = donationChains[indexPath.section].identifier
         if expandedDonationChainsIDs.contains(donationChainID) {
-            expandedDonationChainsIDs.remove(donationChainID)
+            eventHandler?.onDonationChainHideFullInfoRequested(for: donationChainID)
         }
         else {
-            expandedDonationChainsIDs.insert(donationChainID)
+            eventHandler?.onDonationChainShowFullInfoRequested(for: donationChainID)
         }
     }
 
